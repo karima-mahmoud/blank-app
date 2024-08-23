fa275cb2eed4891309c81915d3d6d390d944610d
Karima [ Matrix ] <123845298+karima-mahmoud@users.noreply.github.com>
2024-08-23 19:14:02 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,5 @@ f3= st.number_input('feture 3',min_value=1,max_value=100)
 with open('model2.pkl','rb') as file:
  model= pickle.load(file)
 output=model.predict([[f1,f2,f3]])
+
+st.write(output[0][0])
