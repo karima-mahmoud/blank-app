cb8ba8ccb107acaf73835ac2b537af49b9ad6851
Karima [ Matrix ] <123845298+karima-mahmoud@users.noreply.github.com>
2024-08-23 18:55:02 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pickle
 st.title("car co2 emssion")
 f1= st.number_input('feture 1',min_value=1,max_value=10)
 f2= st.number_input('feture 2',min_value=1,max_value=10)
-f3= st.number_input('feture 3',min_value=1,max_value=10)
+f3= st.number_input('feture 3',min_value=1,max_value=100)
 
 with open('model.pkl','rb') as file:
   model= pickle.load(file)
