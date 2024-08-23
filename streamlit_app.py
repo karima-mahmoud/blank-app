ba8a49eda4dc2c647b03bbb4bf26dbdf7a7ed1fa
Karima [ Matrix ] <123845298+karima-mahmoud@users.noreply.github.com>
2024-08-23 18:48:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title("car co2 emssion")
-st.number_input('feture 1',min_value=1,max_value=10)
-st.number_input('feture 2',min_value=1,max_value=10)
-st.number_input('feture 3',min_value=1,max_value=10)
+f1= st.number_input('feture 1',min_value=1,max_value=10)
+f2= st.number_input('feture 2',min_value=1,max_value=10)
+f3= st.number_input('feture 3',min_value=1,max_value=10)
