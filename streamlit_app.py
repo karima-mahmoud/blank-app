c1cc496fde12aae3d2f15b82b080aa484acfa1d7
Karima [ Matrix ] <123845298+karima-mahmoud@users.noreply.github.com>
2024-08-23 18:45:27 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("🎈 My new app2")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("car co2 emssion")
+st.number_input('feture 1',min_value=1,max_value=10)
